96c695049f83aca6edb698eebefde7b1f971685f
floreseternasoporte-creator <floreseternasoporte@gmail.com>
2026-02-09 00:08:53 -0600

    Remove relato daily story feature

--- api/get-chapters.js
+++
@@ -1,4 +0,0 @@
-const { createHandler } = require('./_netlifyAdapter');
-const { handler } = require('../get-chapters.js');
-
-module.exports = createHandler(handler);
--- api/groq-chat.js
+++
@@ -1,4 +0,0 @@
-const { createHandler } = require('./_netlifyAdapter');
-const { handler } = require('../groq-chat.js');
-
-module.exports = createHandler(handler);
