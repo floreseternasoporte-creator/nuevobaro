1cfb4178276ecaaebc4379107bbada0da29a16e4
floreseternasoporte-creator <floreseternasoporte@gmail.com>
2026-02-09 12:49:06 -0600

    Style home section headers with Uber theme

--- api/upload-story.js
+++
@@ -1,4 +0,0 @@
-const { createHandler } = require('./_netlifyAdapter');
-const { handler } = require('../upload-story.js');
-
-module.exports = createHandler(handler);
--- api/user-stats.js
+++
@@ -1,4 +0,0 @@
-const { createHandler } = require('./_netlifyAdapter');
-const { handler } = require('../user-stats.js');
-
-module.exports = createHandler(handler);
